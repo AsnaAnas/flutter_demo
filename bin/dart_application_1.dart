0797b805845ba6e18826d331ad27356f91331b4b
AsnaAnas <ksasna7@gmail.com>
2024-02-27 13:28:16 +0530

    fibanocci file

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -19,6 +19,6 @@ void main() {
  //list();
  //gcd();
 // factorial();
-armstrong();
+//armstrong();
 }
 
--- bin/fibanocci.dart
+++ bin/fibanocci.dart
@@ -4,5 +4,13 @@ void fibanocci()
 {
   int n1=0,n2=1;
   int lim=int.parse(stdin.readLineSync()!);
-  
+  print(n1);
+  print(n2);
+  for(int i=1;i<lim-1;i++)
+  {
+    int n3=n1+n2;
+    print(n3);
+    n1=n2;
+    n2=n3;
+  }
 }
